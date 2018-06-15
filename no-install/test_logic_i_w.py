61571d925d244d25bedb2bf38faff3adcc31ab03
Fabio Mora <fabiomoi022@gmail.com>
2018-06-15 12:19:51 -0600

    Update PYTEST

--- no-install/test_logic_i_w.py
+++ no-install/test_logic_i_w.py
@@ -49,3 +49,33 @@ class TestLogicIW(object):
             relations)
         expected_result = True
         assert result == expected_result
+
+    def test_related_2(self):
+        """Test Case: hijo is related to primo? -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" abuelo",spa," padre",spa)',
+            '+ etymological_origin_of(" abuelo",spa," tio",spa)',
+            '+ etymological_origin_of(" padre",spa," hijo",spa)',
+            '+ etymological_origin_of(" tio",rus," primo",rus)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_w.related(
+            "hijo",
+            "spa",
+            "primo",
+            "rus",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = False
+        assert result == expected_result
