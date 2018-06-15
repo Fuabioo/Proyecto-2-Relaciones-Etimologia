0435749bdfae4ab059d203911dcf595bfa05b63b
Fabio Mora <fabiomoi022@gmail.com>
2018-06-15 13:43:29 -0600

    Update PYTEST

--- no-install/test_logic_i_i.py
+++ no-install/test_logic_i_i.py
@@ -21,7 +21,7 @@ class TestLogicIW(object):
     """ Test object validation for the logic"""
 
     def test_common_amount_1(self):
-        """Test Case: spa is related to primo? -> Yes"""
+        """Test Case: common amount eng-spa -> Yes"""
         model_test = Model()
         data_pool = [
             '+ etymological_origin_of(" taco",spa," burrito",spa)',
@@ -47,4 +47,207 @@ class TestLogicIW(object):
             model_test.get_logic(),  # data
             relations)
         expected_result = True
+        assert result == expected_result
+
+    def test_common_amount_2(self):
+        """Test Case: common amount eng-spa -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.common_amount(
+            "-",
+            "-",
+            "eng",
+            "spa",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = True
+        assert result == expected_result
+
+    def test_common_1(self):
+        """Test Case: common eng-spa -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.common(
+            "-",
+            "-",
+            "eng",
+            "fin",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = True
+        assert result == expected_result
+
+    def test_common_2(self):
+        """Test Case: common eng-spa -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.common(
+            "-",
+            "-",
+            "eng",
+            "fin",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = True
+        assert result == expected_result
+
+    def test_contributed_most_1(self):
+        """Test Case: contributed_most eng -> Yes"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.contributed_most(
+            "-",
+            "-",
+            "eng",
+            "-",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = 'Result in console.'
+        assert result == expected_result
+
+    def test_contributed_most_2(self):
+        """Test Case: contributed_most fin -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.contributed_most(
+            "-",
+            "-",
+            "eng",
+            "-",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = 'Result in console.'
+        assert result == expected_result
+
+    def test_idiom_list_1(self):
+        """Test Case: idiom_list fin -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.idiom_list(
+            "-",
+            "-",
+            "eng",
+            "-",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = 'Result in console.'
+        assert result == expected_result
+
+    def test_idiom_list_2(self):
+        """Test Case: idiom_list fin -> No"""
+        model_test = Model()
+        data_pool = [
+            '+ etymological_origin_of(" taco",fin," burrito",fin)',
+            '+ etymological_origin_of(" taco",eng," burrito",eng)',
+            '+ etymological_origin_of(" taco",deu," burrito",deu)']
+        model_test.append_data(data_pool)
+        model_test.load(Dummy())
+        relations = {
+            'derived': False,
+            'etymologically': False,
+            'etymology': False,
+            'etymologically_related': False,
+            'has_derived_form': False,
+            'etymological_origin_of': True,
+            'is_derived_from': False,
+            'variant': False}
+        result = logic_i_i.idiom_list(
+            "-",
+            "-",
+            "eng",
+            "-",
+            Dummy(),
+            model_test.get_logic(),  # data
+            relations)
+        expected_result = 'Result in console.'
         assert result == expected_result
